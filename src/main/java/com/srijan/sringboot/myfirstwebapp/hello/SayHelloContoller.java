5256602820f5c9027e8b8ba0e116f5d65b603608
srijan_singh <singhsrijan809@gmail.com>
2024-06-27 08:44:07 +0530

    Say-hello Controller

---
+++ src/main/java/com/srijan/sringboot/myfirstwebapp/hello/SayHelloContoller.java
@@ -0,0 +1,18 @@
+package com.srijan.sringboot.myfirstwebapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloContoller {
+	
+	//'say-hello' => "Hello! What are you learning today?"
+	
+	@RequestMapping("say-hello")
+	@ResponseBody
+	public String sayHello() {
+		return "Hello! What are you learning today?";
+	}
+
+}
