21c508de017166dc1a30731dffea98df18cef4f8
srijan_singh <singhsrijan809@gmail.com>
2024-06-27 08:33:06 +0530

    First Commit

---
+++ src/main/java/com/srijan/sringboot/myfirstwebapp/MyfirstwebappApplication.java
@@ -0,0 +1,14 @@
+package com.srijan.sringboot.myfirstwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstwebappApplication.class, args);
+		System.out.println("Srijan");
+	}
+
+}
