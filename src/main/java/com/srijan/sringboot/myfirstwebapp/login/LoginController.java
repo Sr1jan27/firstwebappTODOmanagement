cdba2adb6da3c0d30126b39a5e1b2310e589014e
srijan_singh <singhsrijan809@gmail.com>
2024-06-30 13:45:15 +0530

    starting to build login page

--- src/main/java/com/srijan/sringboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/srijan/sringboot/myfirstwebapp/login/LoginController.java
@@ -1,22 +1,12 @@
 package com.srijan.sringboot.myfirstwebapp.login;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 @Controller
 public class LoginController {
-	
-//	@RequestMapping("/login")
-//	public String login() {
-//		return "login";
-//	}
-	
+
 	@RequestMapping("/login")
-	public String gotoLoginPage(@RequestParam String name, ModelMap model) {
-		
-		model.put("name", name);
+	public String gotoLoginPage() {
 		return "login";
 	}
 
