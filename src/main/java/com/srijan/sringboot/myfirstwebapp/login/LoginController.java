ecb70bca8d01f02e3194ce1d84eeaf12731f86b8
srijan_singh <singhsrijan809@gmail.com>
2024-06-30 14:24:41 +0530

    checking with authentication

--- src/main/java/com/srijan/sringboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/srijan/sringboot/myfirstwebapp/login/LoginController.java
@@ -1,12 +1,38 @@
 package com.srijan.sringboot.myfirstwebapp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
+	
+	private AuthenticationService authenticationService;
+	
+	
 
-	@RequestMapping("/login")
+	public LoginController(AuthenticationService authenticationService) {
+		super();
+		this.authenticationService = authenticationService;
+	}
+
+	@RequestMapping(value="/login", method = RequestMethod.GET)
 	public String gotoLoginPage() {
+		return "login";
+	}
+	
+	@RequestMapping(value="/login", method = RequestMethod.POST)
+	public String gotoWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {
+		
+		if(authenticationService.authenticate(name, password)) {
+
+			model.put("name", name);
+			model.put("password", password);
+			
+			return "Welcome";
+		}
+		model.put("errormessgae", "Invalid Credentials, Please Try Again !!");
 		return "login";
 	}
 
---
+++ src/main/java/com/srijan/sringboot/myfirstwebapp/todo/Todo.java
@@ -0,0 +1,5 @@
+package com.srijan.sringboot.myfirstwebapp.todo;
+
+public class Todo {
+
+}
