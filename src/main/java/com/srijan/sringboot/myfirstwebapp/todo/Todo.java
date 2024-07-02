65ace0bb74bb6abb48a804ea3e69a3c13bc4aa4a
srijan_singh <singhsrijan809@gmail.com>
2024-07-02 08:29:43 +0530

    Step 23

--- src/main/java/com/srijan/sringboot/myfirstwebapp/todo/Todo.java
+++ src/main/java/com/srijan/sringboot/myfirstwebapp/todo/Todo.java
@@ -2,6 +2,8 @@ package com.srijan.sringboot.myfirstwebapp.todo;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.Size;
+
 
 public class Todo {
 
@@ -18,6 +20,7 @@ public class Todo {
 
 	private String username;
 
+	@Size(min=10, message="Enter atleast 10 characters!")
 	private String description;
 
 	private LocalDate targetDate;
