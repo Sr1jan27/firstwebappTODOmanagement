56a029aef0fb9cedb8171e1ddd8975d5fea397ac
srijan_singh <singhsrijan809@gmail.com>
2024-07-05 00:53:56 +0530

    refactor

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,7 +2,7 @@ insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
 values(10001,'Srijan', 'Get AWS Certified', CURRENT_DATE(), false);
 
 
-nsert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
 values(10002,'Srijan', 'Get Azure Certified', CURRENT_DATE(), false);
 
 insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
