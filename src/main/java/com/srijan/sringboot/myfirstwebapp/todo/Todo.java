e03a17f506b58f42a24218b78d7790d8e9ea650e
srijan_singh <singhsrijan809@gmail.com>
2024-07-05 00:53:12 +0530

    adding h2

--- src/main/java/com/srijan/sringboot/myfirstwebapp/todo/Todo.java
+++ src/main/java/com/srijan/sringboot/myfirstwebapp/todo/Todo.java
@@ -2,9 +2,12 @@ package com.srijan.sringboot.myfirstwebapp.todo;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
-
+@Entity
 public class Todo {
 
 	public Todo(int id, String username, String description, LocalDate targetDate, boolean done) {
@@ -16,6 +19,8 @@ public class Todo {
 		this.done = done;
 	}
 
+	@Id
+	@GeneratedValue
 	private int id;
 
 	private String username;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001,'Srijan', 'Get AWS Certified', CURRENT_DATE(), false);
+
+
+nsert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002,'Srijan', 'Get Azure Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003,'Srijan', 'Get GCP Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10004,'Srijan', 'Learn DevOps', CURRENT_DATE(), false);
